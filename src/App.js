b3309a3a7f26058fdfb7c4f15455d02e44b798d6
Andres Galindo <agalin920@gmail.com>
2019-04-01 13:20:20 -0600

    Added HeaderNav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import HeaderNav from './containers/HeaderNav/HeaderNav';
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <HeaderNav/>
+      </div>
     );
   }
 }
 
 export default App;
-~~
